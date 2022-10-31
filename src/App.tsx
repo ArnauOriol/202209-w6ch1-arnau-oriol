ebd96a75728297d55baa05d5ee853800555e582c
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-10-31 21:27:18 +0100

    Add ToDo component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Button from "./components/Button/Button";
 const App = () => {
   return (
     <>
-      <p>jelou</p>
       <Button text="hola" action={() => console.log("hola")} />
     </>
   );
---
+++ src/components/ToDo/ToDo.tsx
@@ -0,0 +1,19 @@
+import Task from "../../data/types";
+import { removeTaskActionCreator } from "../../redux/features/taskSlice";
+import Button from "../Button/Button";
+
+interface TaskProps {
+  task: Task;
+}
+
+const ToDo = ({ task: { id, name, done } }: TaskProps): JSX.Element => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <input type="checkbox" name="" id="" />
+      <Button text="DELETE" action={() => removeTaskActionCreator(id)} />
+    </div>
+  );
+};
+
+export default ToDo;
