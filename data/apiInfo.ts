7deba0c2f2516e0abe3d6b406d6b83890d3f183a
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-10-31 19:27:59 +0100

    Add types and fake API info

---
+++ data/apiInfo.ts
@@ -0,0 +1,16 @@
+import Task from "./types";
+
+const initialArrayOfTasks: Task[] = [
+  {
+    id: 1,
+    name: "fregar platos",
+    done: false,
+  },
+  {
+    id: 2,
+    name: "lavar ropa",
+    done: false,
+  },
+];
+
+export default initialArrayOfTasks;
